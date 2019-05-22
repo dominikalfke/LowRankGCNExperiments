e0140bf298be66a714b47902377374f73c62eeaa
Dominik Alfke <alfke@mathematik.tu-chemnitz.de>
2019-05-22 16:24:36 +0200

    Corrected dataset filename in cars experiments

--- cars/cars-reduced20-linear.jl
+++ cars/cars-reduced20-linear.jl
@@ -4,7 +4,7 @@ using Random
 
 println("\nRUNNING EXPERIMENT SCRIPT: " * (@__FILE__))
 
-datasetFile = (@__DIR__) * "/mushrooms-dataset.jld"
+datasetFile = (@__DIR__) * "/cars-dataset.jld"
 
 numTrainingIter = 1000
 numRuns = 100
